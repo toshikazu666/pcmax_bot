ff6f61a91c6d5b04cb25d2fcba469519ee4178eb
Toshikazu <toshikazu.flight666@gmail.com>
2019-12-22 16:36:30 +0900

    add city code map generator

--- interface/livedoor.py
+++ interface/livedoor.py
@@ -1,12 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
 class Livedoor:
-    def get_today_weather(self, url, city):
-        # LiveDoor API から今日の天気情報を取得する
+    def get_tomorrow_weather(self, weather_forecast_url, city):
+        # LiveDoor API から翌日の天気情報を取得する
         payload = {'city': city}
-        res = requests.get(url, params=payload)
+        res = requests.get(weather_forecast_url, params=payload)
         forecast = res.json()['forecasts'][1]
-        # たまに取れないことがあるので、取れない場合は例外を捕捉して"--"とする
+        # タイミングによって取れないことがあるので、取れない場合は例外を捕捉して"--"とする
         try:
             weather = forecast['telop']
         except:
@@ -23,3 +24,10 @@ class Livedoor:
             print('Can not get min temperature on %s'%city)
             min_t = '--'
         return {'weather': weather, 'max': max_t, 'min': min_t}
+
+    def get_city_code_dict(self, city_code_map_url):
+        # 市町村とコードの対応付を辞書にして返す
+        res = requests.get(city_code_map_url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        city_infos = soup.find_all('city')
+        return {str(city_info.get('title')): str(city_info.get('id')) for city_info in city_infos}
