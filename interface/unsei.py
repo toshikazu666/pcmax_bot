864d30941544e34fcba95c1233fcd0a34c368204
Toshikazu <toshikazu.flight666@gmail.com>
2020-02-15 08:34:26 +0900

    new: todays unsei template

---
+++ interface/unsei.py
@@ -0,0 +1,6 @@
+import requests
+
+class Unsei:
+    def get_unsei(self, url, today, umare_month):
+        # APIから今日の○月生まれの運勢を取得する
+        return requests.get(url).json()['horoscope'][today][umare_month]
