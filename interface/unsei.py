0380cdaf42cf18830d06a095e6edf760fa747b86
Toshikazu <toshikazu.flight666@gmail.com>
2020-02-15 10:30:53 +0900

    bug: modify month-array index

--- interface/unsei.py
+++ interface/unsei.py
@@ -3,4 +3,4 @@ import requests
 class Unsei:
     def get_unsei(self, url, today, umare_month):
         # APIから今日の○月生まれの運勢を取得する
-        return requests.get(url).json()['horoscope'][today][umare_month]
+        return requests.get(url).json()['horoscope'][today][umare_month-1]
